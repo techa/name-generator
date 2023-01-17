2c21b656fa5dbec78a56ef0b481810beff1857b6
techa <techa@users.noreply.github.com>
2023-01-17 13:30:40 +0000

    deploy: fac0032f1f80408ea49d90a5b8d9a02d8e5c411e

---
+++ internal/immutable/chunks/2-04d97e00.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-5436b648.js";export{t as component};
--- internal/immutable/chunks/2-bb208365.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-033c68aa.js";export{t as component};
