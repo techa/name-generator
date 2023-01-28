2a05a0b9aa9bf24ae5113f9889b639cae9c62b6d
techa <techa@users.noreply.github.com>
2023-01-28 16:10:01 +0000

    deploy: 6c63b3a77c2faedf30e37e67d57772057d0ef05b

--- internal/immutable/chunks/0-c180e53c.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_layout.svelte-bcc37b0b.js";export{t as component};
--- internal/immutable/chunks/2-20e42da8.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-e484413a.js";export{t as component};
