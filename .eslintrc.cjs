2a548bef08184537e3fe9e0d721acbed0700e733
techa <techa27@gmail.com>
2024-09-25 14:43:51 +0900

    chore: update eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,22 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:svelte/recommended',
+	],
+	plugins: ['@typescript-eslint'],
+	ignorePatterns: ['*.cjs', '*.html'],
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser',
+			},
+		},
+	],
 	rules: {
 		'@typescript-eslint/no-empty-interface': [
 			'error',
@@ -20,14 +32,13 @@ module.exports = {
 			'error',
 			{ ignoreDeclarationMerge: true },
 		],
-		'no-mixed-spaces-and-tabs': ['off'],
-	},
-	settings: {
-		'svelte3/typescript': () => require('typescript'),
+		'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
 	},
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020,
+		project: './tsconfig.json',
+		extraFileExtensions: ['.svelte'],
 	},
 	env: {
 		browser: true,
