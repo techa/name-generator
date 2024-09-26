e5db5fa7f79eb99e7800a771b58a63714dddd0f5
techa <techa27@gmail.com>
2024-09-26 15:54:30 +0900

    chore: ts-node -> tsx

--- scripts/kanaToExp.ts
+++ scripts/kanaToExp.ts
@@ -1,9 +1,9 @@
-// tsc | node ./.build/scripts/kanaToExp.js
+// pnpm tsx ./scripts/kanaToExp.js
 import { readFileSync, writeFileSync } from 'fs';
-import { Translator } from '../src/lib/Translator.js';
+import { Phonation } from '../src/lib/Phonation.js';
 import { types, langs } from './constants.js';
 
-const translator = new Translator();
+const phonation = new Phonation();
 
 for (const type of types) {
 	for (const lang of langs) {
@@ -16,8 +16,8 @@ for (const type of types) {
 				// Avoid redundancy
 				if (!acc.includes(v)) acc.push(v);
 				return acc;
-			}, [])
-			.map((kana) => translator.fromKana(kana))
+			}, [] as string[])
+			.map((kana) => phonation.fromKana(kana))
 			.join('\n');
 
 		writeFileSync(`./resource/exp/${type}-${lang}.txt`, text);
@@ -28,12 +28,12 @@ const others = ['jw', 'mt'];
 for (const other of others) {
 	const text = readFileSync(`./resource/kana/other-${other}.txt`, 'utf-8')
 		.split('\n')
-		.map((kana) => translator.fromKana(kana))
+		.map((kana) => phonation.fromKana(kana))
 		.join('\n');
 
 	writeFileSync(`./resource/exp/other-${other}.txt`, text);
 }
 
-if (translator.fromKanaMissings.size) {
-	console.log(translator.fromKanaMissings);
+if (phonation.fromKanaMissings.size) {
+	console.log(phonation.fromKanaMissings);
 }
